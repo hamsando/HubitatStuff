e01de587722a638098aa051df03273c532c004f0
hamsando <hamsando@users.noreply.github.com>
2020-02-25 14:47:18 -0800

    Update Stat_Holiday_Switch.groovy
    *Changed the original state name from typeOfDate to Today - which still shows whether today is a work day and if not, what holiday/weekend it is.
    *Added matching Attribute TodayIs to make it easier to integrate with RM
    
    *Added a new state name Tomorrow, which shows whether tomorrow is a work day and if not, what holiday/weekend it is.
    *Added matching Attribute TomorrowIs to make it easier to integrate with RM
    
    *Added Easter Monday (and made it optional via driver preference toggle)
    
    *Added ability to enable/disable debug logging.

--- Stat_Holiday_Switch.groovy
+++ Stat_Holiday_Switch.groovy
@@ -13,23 +13,29 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  V0.1 Initial Release
+ *  V0.2 Updates States. Added Attributes. Added Easter Monday. Added abilitly to disable debug logging.
  */
  
 metadata {
-	definition (name: "Stat Holiday Switch", namespace: "hamsando", author: "ham") { 
+    definition (name: "Stat Holiday Switch", namespace: "hamsando", author: "ham") { 
         capability "Switch"
         capability "Refresh"       
-		capability "Initialize"
-		command "on"
-		command "off"        
-	}
+        capability "Initialize"
+        attribute "TodayIs", "string"
+        attribute "TomorrowIs", "string"
+        
+        command "on"
+        command "off"        
+    }
 
     preferences {
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+        input name: "celebrateEasterMonday", type: "bool", title: "Easter Monday?", defaultValue: false
     }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"	
+    logDebug "Installed with settings: ${settings}"    
     initialize()
 }
 
@@ -38,150 +44,175 @@ def refresh() {
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-    initialize()	    
+    logDebug "Updated with settings: ${settings}"
+    initialize()        
 }
 
-def initialize() {	 
-    //def tz = TimeZone.getTimeZone('America/Los_Angeles') 
-	//def rundate = d.format('dd/MM/yyyy HH:mm', tz)
+def initialize() {     
     off()    
-    state.typeOfDate = "Workday"
+    state.clear()
+    state.Today = "Work day"
+    state.Tomorrow = "Work day"
+    
     try {
         unsubscribe()
     } catch (unsubError) {
-        log.info(unsubError)
+        logDebug(unsubError)
     } 
     
     try {
         unschedule()
     } catch (unschError) {
-        log.info(unschError)
+        logDebug(unschError)
     }
-    schedule("0 5 0 1/1 * ? *", doDaily)
+    schedule("0 5 0 1/1 * ? *", doDaily)    
     doDaily()
 }
 
 def on() {
-    sendEvent(name: "switch", value: "on", isStateChange: true)	    
+    sendEvent(name: "switch", value: "on", isStateChange: true)        
 }
 
 def off() {
-    sendEvent(name: "switch", value: "off", isStateChange: true)	    
+    sendEvent(name: "switch", value: "off", isStateChange: true)        
 }
 
 def doDaily() {
-	if (todayIsAHoliday())  {    
-    	log.debug "Today is not a work day!"
+    if (todayIsAHoliday(0))  {    
+        log.info "Today is not a work day!"
         on()
     } else {
-    	log.debug "Today is a work day"
+        log.info "Today is a work day"
         off()
-	}
+    }
 
+    todayIsAHoliday(1)
+    log.info "Tomorrow is: " + state.Tomorrow
 }
 
-def todayIsAHoliday() {
-	def thisDay = new Date().clearTime()    
-    log.debug("Today is: " + thisDay)
-    state.typeOfDate = "Workday"
-	if (isWeekend(thisDay)) {
-    	state.typeOfDate = "Weekend"
-    	return true
+def todayIsAHoliday(int tomorrow) {
+    notFound = true
+    dayType = "Workday" 
+    
+    todayTomorrow = "Tomorrow"
+    if (tomorrow == 0) {
+        todayTomorrow = "Today"
+    }
+    
+    def thisDay = new Date().clearTime() + tomorrow
+    logDebug(todayTomorrow + " is: " + thisDay)
+        
+    if (isWeekend(thisDay)) {
+        dayType = "Weekend"
+        notFound = false
     } else {
-    	if (isNYD(thisDay)) {
-        	state.typeOfDate = "New Years Day"
-        	return true
+        if (notFound && isNYD(thisDay)) {
+            dayType = "New Years Day"
+            notFound = false
         }
         
-        if (isFD(thisDay)) {
-        	state.typeOfDate = "Family Day"
-        	return true
+        if (notFound && isFD(thisDay)) {
+            dayType = "Family Day"
+            notFound = false
         }
 
-		if (isGF(thisDay)) {
-        	state.typeOfDate = "Good Friday"
-        	return true
+        if (notFound && isGF(thisDay)) {
+            dayType = "Good Friday"
+            notFound = false
         }
 
-        if (isVD(thisDay)) {
-            state.typeOfDate = "Victoria Day"
-        	return true
+        if (notFound && (celebrateEasterMonday) && isEM(thisDay)) {
+            dayType = "Easter Monday"
+            notFound = false
         }
 
-        if (isCD(thisDay)) {
-        	state.typeOfDate = "Canada Day"        
-        	return true
+        if (notFound && isVD(thisDay)) {
+            dayType = "Victoria Day"
+            notFound = false
         }
 
-        if (isBC(thisDay)) {
-        	state.typeOfDate = "BC Day"        
-        	return true
+        if (notFound && isCD(thisDay)) {
+            dayType = "Canada Day"        
+            notFound = false
         }
 
-        if (isLD(thisDay)) {
-        	state.typeOfDate = "Labor Day"        
-        	return true
+        if (notFound && isBC(thisDay)) {
+            dayType = "BC Day"        
+            notFound = false
         }
 
-        if (isT(thisDay)) {
-        	state.typeOfDate = "Thanksgiving"        
-        	return true
+        if (notFound && isLD(thisDay)) {
+            dayType = "Labor Day"        
+            notFound = false
         }
 
-        if (isRD(thisDay)) {
-        	state.typeOfDate = "Rememberance Day"        
-        	return true
+        if (notFound && isT(thisDay)) {
+            dayType = "Thanksgiving"        
+            notFound = false
         }
 
-        if (isX(thisDay)) {
-        	state.typeOfDate = "Xmas"        
-        	return true
+        if (notFound && isRD(thisDay)) {
+            dayType = "Rememberance Day"        
+            notFound = false
         }
 
-        if (isBD(thisDay)) {
-        	state.typeOfDate = "Boxing Day"        
-        	return true
+        if (notFound && isX(thisDay)) {
+            dayType = "Xmas"        
+            notFound = false
+        }
+
+        if (notFound && isBD(thisDay)) {
+            dayType = "Boxing Day"        
+            notFound = false
         }        
     }
-    return false
+    
+    if (tomorrow == 0) {
+        state.Today = dayType
+        sendEvent(name: "TodayIs", value: dayType, displayed:true, isStateChange: true)
+    } else {
+        state.Tomorrow = dayType
+        sendEvent(name: "TomorrowIs", value: dayType, displayed:true, isStateChange: true)
+    }
+    
+    return !notFound
 }
 
 def isWeekend(Date d) {
-	log.debug "Checking weekend"
-	def df = new java.text.SimpleDateFormat("EEEE")
-    def dayOfWeek = df.format(d)    	
+    logDebug "Checking weekend"
+    def df = new java.text.SimpleDateFormat("EEEE")
+    def dayOfWeek = df.format(d)        
     if ((dayOfWeek=="Saturday")|| (dayOfWeek=="Sunday")) 
-    	return true
+        return true
     else 
-    	return false
+        return false
        
 }
 
 def isNYD(Date d) {
-	log.debug "Checking New Years Day"    
-	//if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 0, 1))) {
+    logDebug "Checking New Years Day"    
+    //if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 0, 1))) {
     if (d == findFollowingMonday(new Date().parse('MM/dd/yyyy', "01/01/" + d[Calendar.YEAR]))) {
-    	return true
+        return true
     } else {
-    	return false    
+        return false    
     }
 }
 
 def isFD(Date d) {
-	log.debug "Checking family day"
-	if (d == findxDay(3, "Monday",2)) 
-    	return true
+    logDebug "Checking family day"
+    if (d == findxDay(3, "Monday",2)) 
+        return true
      else 
-    	return false       	
+        return false           
 }
 
-def isGF(Date cd) {	
-	log.debug "Checking Good Friday"
+def isGF(Date cd) {    
+    logDebug "Checking Good Friday"
     int y = cd[Calendar.YEAR]
     int c = (y/100)
     int n = y - 19 * (y/19).toInteger()
-	int k = (( c - 17 ) / 25)
+    int k = (( c - 17 ) / 25)
     int i = c - (c / 4) - (( c - k ) / 3).toInteger() + 19 * n + 15;
     i = i - 30 * ( i / 30 ).toInteger();
     i = i - ( i / 28 ).toInteger() * ( 1 - ( i / 28 ).toInteger() * ( 29 / ( i + 1 ) ).toInteger() * ( ( 21 - n ) / 11 ).toInteger());
@@ -190,125 +221,150 @@ def isGF(Date cd) {
     int l = i - j;
     int m = 3 + (( l + 40 ) / 44).toInteger();
     int d = l + 28 - 31 * ( m / 4 ).toInteger();    
-	
-	Date tmpDate =  new Date(y-1900,m-1,d)	
-	log.debug "Easter is $tmpDate"
-	tmpDate = tmpDate-2
-	log.debug "Good Friday is $tmpDate"
+    
+    Date tmpDate =  new Date(y-1900,m-1,d)    
+    logDebug "Easter is $tmpDate"
+    tmpDate = tmpDate-2
+    logDebug "Good Friday is $tmpDate"
     if (cd == tmpDate) 
-    	return true
+        return true
     else
-    	return false
+        return false
+}
+
+def isEM(Date cd) {
+    logDebug "Checking Easter Monday"
+    int y = cd[Calendar.YEAR]
+    int c = (y/100)
+    int n = y - 19 * (y/19).toInteger()
+    int k = (( c - 17 ) / 25)
+    int i = c - (c / 4) - (( c - k ) / 3).toInteger() + 19 * n + 15;
+    i = i - 30 * ( i / 30 ).toInteger();
+    i = i - ( i / 28 ).toInteger() * ( 1 - ( i / 28 ).toInteger() * ( 29 / ( i + 1 ) ).toInteger() * ( ( 21 - n ) / 11 ).toInteger());
+    int j = y + (y / 4).toInteger() + i + 2 - c + (c / 4).toInteger();
+    j = j - 7 * ( j / 7 ).toInteger()
+    int l = i - j;
+    int m = 3 + (( l + 40 ) / 44).toInteger();
+    int d = l + 28 - 31 * ( m / 4 ).toInteger();    
+    
+    Date tmpDate =  new Date(y-1900,m-1,d)    
+    logDebug "Easter is $tmpDate"
+    tmpDate = tmpDate+1
+    logDebug "Easter Monday is $tmpDate"
+    if (cd == tmpDate) 
+        return true
+    else
+        return false
 }
 
 def isVD(Date d) {
-	log.debug "Checking Victoria Day"
-	def day = ""
-	def Z=0    
+    logDebug "Checking Victoria Day"
+    def day = ""
+    def Z=0    
     def year = d[Calendar.YEAR]
     def df = new java.text.SimpleDateFormat("EEEE")
     df.setTimeZone(location.timeZone)
     
-	for (int x=1; x <= 7; x++) {
-    	Z = 25-x    	
-    	day = df.format(new Date().parse('MM/dd/yyyy', "5/$Z/$year"))        
+    for (int x=1; x <= 7; x++) {
+        Z = 25-x        
+        day = df.format(new Date().parse('MM/dd/yyyy', "5/$Z/$year"))        
         if (day == "Monday") {        
-        	if (d == new Date().parse('MM/dd/yyyy', "5/$Z/$year")) 
-        		return true
+            if (d == new Date().parse('MM/dd/yyyy', "5/$Z/$year")) 
+                return true
             else 
-            	return false
+                return false
         }
         
     }
 }
 
 def isCD(Date d) {
-	log.debug "Checking Canada Day"
+    logDebug "Checking Canada Day"
     if (d == findFollowingMonday(new Date().parse('MM/dd/yyyy', "07/01/" + d[Calendar.YEAR]))) 
-    	return true
+        return true
     else
-    	return false    
+        return false    
 }
 
 def isBC(Date d) {
-	log.debug "Checking BC Day"
-	if (d == findxDay(1, "Monday",8)) 
-    	return true
+    logDebug "Checking BC Day"
+    if (d == findxDay(1, "Monday",8)) 
+        return true
      else 
-    	return false       	
+        return false           
 }
 
 def isLD(Date d) {
-	log.debug "Checking Labor Day"
-	if (d == findxDay(1, "Monday",9)) 
-    	return true
+    logDebug "Checking Labor Day"
+    if (d == findxDay(1, "Monday",9)) 
+        return true
      else 
-    	return false       	
+        return false           
 }
 
 def isT(Date d) {
-	log.debug "Checking Thanksgiving"
-	if (d == findxDay(2, "Monday",10)) 
-    	return true
+    logDebug "Checking Thanksgiving"
+    if (d == findxDay(2, "Monday",10)) 
+        return true
      else 
-    	return false       	
+        return false           
 }
 
 def isRD(Date d) {
-	log.debug "Checking Rememberance Day"
+    logDebug "Checking Rememberance Day"
     if (d == findFollowingMonday(new Date().parse('MM/dd/yyyy', "11/11/" + d[Calendar.YEAR]))) 
-	//if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 11, 11))) 
-    	return true
+    //if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 11, 11))) 
+        return true
     else
-    	return false    
+        return false    
 }
 
 def isX(Date d) {
-	log.debug "Checking Xmas"
+    logDebug "Checking Xmas"
     if (d == findFollowingMonday(new Date().parse('MM/dd/yyyy', "12/25/" + d[Calendar.YEAR]))) 
-	//if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 12, 25))) 
-    	return true
+    //if (d == findFollowingMonday(new Date(d[Calendar.YEAR], 12, 25))) 
+        return true
     else
-    	return false    
+        return false    
 }
 
 def isBD(Date d) {
-	log.debug "Checking Boxing Day"
+    logDebug "Checking Boxing Day"
     def bd = findFollowingMonday(new Date().parse('MM/dd/yyyy', "12/25/" + d[Calendar.YEAR])) + 1
     //def bd = findFollowingMonday(new Date(d[Calendar.YEAR], 12, 25)) + 1 
-	if (d == findFollowingMonday(bd)) 
-    	return true
+    if (d == findFollowingMonday(bd)) 
+        return true
     else
-    	return false    
+        return false    
 }
 
 def findFollowingMonday(Date d) {
-	log.debug "  Checking following Monday for " + d
-	def df = new java.text.SimpleDateFormat("EEEE")
+    logDebug "  Checking following Monday for " + d
+    def df = new java.text.SimpleDateFormat("EEEE")
     // Ensure the new date object is set to local time zone
     df.setTimeZone(location.timeZone)
     def day = df.format(d)
-    log.debug("  Day of the week of actual holiday: " + day)
+    logDebug("  Day of the week of actual holiday: " + day)
     if (day == "Saturday") {
-        log.debug("  Day we celebrate on: " + (d+2))
-    	return d+2
+        logDebug("  Day we celebrate on: " + (d+2))
+        return d+2
     } else if (day == "Sunday") {
-        log.debug("  Day we celebrate on: " + (d+1))
-    	return d+1
+        logDebug("  Day we celebrate on: " + (d+1))
+        return d+1
     } else {
-        log.debug("  Day we celebrate on: " + (d))
-    	return d
+        logDebug("  Day we celebrate on: " + (d))
+        return d
     }            
 }
 
 def findxDay(int nth, String dow, int month) {
-	log.debug "  Checking $nth $dow of month $month"
+    logDebug "  Checking $nth $dow of month $month"
     def currentDate = new Date()
     
     def year = currentDate[Calendar.YEAR]
     def futureMonth = month+1    
-	def lastDayOfMonth = new Date().parse('MM/dd/yyyy', "$futureMonth/1/$year") - 1    
-    //log.debug "Last Day of Month: $lastDayOfMonth[Calendar.DATE]"
+    def lastDayOfMonth = new Date().parse('MM/dd/yyyy', "$futureMonth/1/$year") - 1    
+    //logDebug "Last Day of Month: $lastDayOfMonth[Calendar.DATE]"
     
     def day = 1
     def counter=0    
@@ -316,17 +372,21 @@ def findxDay(int nth, String dow, int month) {
     def df = new java.text.SimpleDateFormat("EEEE")
     def dayOfWeek
     for (int d=day; d <= lastDayOfMonth[Calendar.DATE]; d++) {
-		tmpDate = new Date().parse('MM/dd/yyyy', "$month/$d/$year")
-        //log.debug tmpDate        
-    	dayOfWeek = df.format(tmpDate)   
-        //log.debug dayOfWeek
-    	if (dayOfWeek==dow) {
-        	counter++
+        tmpDate = new Date().parse('MM/dd/yyyy', "$month/$d/$year")
+        //logDebug tmpDate        
+        dayOfWeek = df.format(tmpDate)   
+        //logDebug dayOfWeek
+        if (dayOfWeek==dow) {
+            counter++
             if (counter == nth) {
-                log.debug("  Day we celebrate on: " + tmpDate)
-            	return tmpDate
+                logDebug("  Day we celebrate on: " + tmpDate)
+                return tmpDate
             }
         }        
     }
     return false
+}
+
+def logDebug(value){
+    if (logEnable) log.debug(value)
 }
